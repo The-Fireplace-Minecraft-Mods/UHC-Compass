4efaf4aa654b7b9cdc759195a42507cac3bf4950
The_Fireplace <the.f1repl4ce@gmail.com>
2017-01-24 17:53:26 -0600

    Made it easy to copy own coordinates from chat

--- src/main/java/the_fireplace/uhccompass/KeyHandler.java
+++ src/main/java/the_fireplace/uhccompass/KeyHandler.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
 import the_fireplace.uhccompass.render.GuiCompassMoving;
+import the_fireplace.uhccompass.render.GuiCoords;
 
 /**
  * @author The_Fireplace
@@ -14,12 +15,13 @@ import the_fireplace.uhccompass.render.GuiCompassMoving;
 public class KeyHandler {
     //Key Index
     public static final int MOVECOMPASS = 0;
+    public static final int QUICKCOORDS = 1;
     //Descriptions, use language file to localize later
     private static final String[] desc =
-            {"key.movecompass.desc"};
+            {"key.movecompass.desc", "key.quickcoords.desc"};
     //Default Key Values
     private static final int[] keyValues =
-            {Keyboard.KEY_EQUALS};
+            {Keyboard.KEY_EQUALS, Keyboard.KEY_APOSTROPHE};
     private final KeyBinding[] keys;
     public KeyHandler(){
         keys = new KeyBinding[desc.length];
@@ -33,6 +35,9 @@ public class KeyHandler {
     public void onKeyInput(InputEvent.KeyInputEvent event){
         if(keys[MOVECOMPASS].isPressed()){
             Minecraft.getMinecraft().displayGuiScreen(new GuiCompassMoving());
+        }
+        if(keys[QUICKCOORDS].isPressed()){
+            Minecraft.getMinecraft().displayGuiScreen(new GuiCoords());
         }
     }
 
---
+++ src/main/java/the_fireplace/uhccompass/render/GuiCoords.java
@@ -0,0 +1,89 @@
+package the_fireplace.uhccompass.render;
+
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.util.ChatComponentTranslation;
+import org.lwjgl.input.Keyboard;
+import the_fireplace.uhccompass.KeyHandler;
+import the_fireplace.uhccompass.UHCCompass;
+
+import java.io.IOException;
+
+/**
+ * @author The_Fireplace
+ */
+public class GuiCoords extends GuiChat {
+    private GuiTextField xCoord;
+    private GuiTextField zCoord;
+    @Override
+    public void initGui() {
+        super.initGui();
+        this.inputField.setCanLoseFocus(true);
+        this.inputField.setFocused(false);
+        xCoord = new GuiTextField(0, this.fontRendererObj, 2, height/2-20-this.inputField.height, 100, 20);
+        xCoord.setFocused(true);
+        xCoord.setMaxStringLength(16);
+        zCoord = new GuiTextField(1, this.fontRendererObj, 104, height/2-20-this.inputField.height, 100, 20);
+        zCoord.setMaxStringLength(16);
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        xCoord.drawTextBox();
+        zCoord.drawTextBox();
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException
+    {
+        if (keyCode == mc.gameSettings.keyBindInventory.getKeyCode() || keyCode == Keyboard.KEY_RETURN || keyCode == UHCCompass.instance.keyHandler.getKeyCode(KeyHandler.QUICKCOORDS))
+        {
+            try{
+                int x = Integer.parseInt(xCoord.getText());
+                int z = Integer.parseInt(zCoord.getText());
+                UHCCompass.TARGETX_PROPERTY.set(x);
+                UHCCompass.TARGETZ_PROPERTY.set(z);
+            }catch(Exception e){
+                mc.thePlayer.addChatMessage(new ChatComponentTranslation("uhccompass.invalidcoords"));
+            }
+            UHCCompass.syncConfig();
+
+            this.mc.displayGuiScreen(null);
+
+            if (this.mc.currentScreen == null)
+            {
+                this.mc.setIngameFocus();
+            }
+        } else if(keyCode == Keyboard.KEY_TAB || keyCode == Keyboard.KEY_SPACE){
+            xCoord.setFocused(!xCoord.isFocused());
+            zCoord.setFocused(!zCoord.isFocused());
+        }else if(Character.isDigit(typedChar) || typedChar == '-' || keyCode == Keyboard.KEY_BACK || keyCode == Keyboard.KEY_LEFT || keyCode == Keyboard.KEY_RIGHT){
+            xCoord.textboxKeyTyped(typedChar, keyCode);
+            zCoord.textboxKeyTyped(typedChar, keyCode);
+        }else if (keyCode == 201)
+        {
+            this.mc.ingameGUI.getChatGUI().scroll(this.mc.ingameGUI.getChatGUI().getLineCount() - 1);
+        }
+        else if (keyCode == 209)
+        {
+            this.mc.ingameGUI.getChatGUI().scroll(-this.mc.ingameGUI.getChatGUI().getLineCount() + 1);
+        }
+        super.keyTyped(typedChar, keyCode);
+    }
+
+    @Override
+    public void updateScreen()
+    {
+        xCoord.updateCursorCounter();
+        zCoord.updateCursorCounter();
+        super.updateScreen();
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        xCoord.mouseClicked(mouseX, mouseY, mouseButton);
+        zCoord.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+}
